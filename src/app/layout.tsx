aa684cfa6eca709250fcf2dc2646dbb6924db659
Truimo <xiaomo@20mo.cn>
2024-09-29 13:26:35 +0800

    fix: maximum scale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const viewport: Viewport = {
     width: 'device-width',
     initialScale: 1,
     minimumScale: 1,
-    maximumScale: 1,
+    maximumScale: 5,
     userScalable: true,
 }
 
