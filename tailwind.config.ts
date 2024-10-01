68c93a49f70d31c85ade5359975e93e2b3042c8e
Truimo <Xiaomo@20mo.cn>
2024-10-01 10:43:54 +0800

    feat: tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,13 @@ import type {Config} from "tailwindcss";
 
 const config: Config = {
     content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        './src/**/*.{js,jsx,ts,tsx}',
     ],
     theme: {
         extend: {
             colors: {
-                background: "var(--background)",
-                foreground: "var(--foreground)",
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
             },
             maxWidth: {
                 '4/5': '80%',
