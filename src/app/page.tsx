e0f5029f0dfd477d41b58d45babecd8b50863d1b
Truimo <xiaomo@20mo.cn>
2024-10-05 10:59:26 +0800

    feat: primary color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ import Link from "next/link";
 export default function Home() {
     return (
         <div className="font-[family-name:var(--font-geist-sans)]">
+            <nav className="mx-auto max-w-4/5 lg:w-1/2">
+                <div className="my-4 text-xl">
+                    <Link href="/" className="font-bold">ME</Link>
+                </div>
+            </nav>
             <header className="font-normal static top-8">
                 <div className="m-8 text-center">
                     <Image src="/avatar.jpg" alt="avatar" height="96" width="96" className="rounded-full w-24 h-24 inline"></Image>
@@ -11,23 +16,27 @@ export default function Home() {
                     <p>Meeting you is the best of the best.</p>
                 </div>
             </header>
-            <main className="grid grid-cols-1 gap-y-2 mx-auto max-w-4/5 lg:w-1/2">
-                <p>Hello, I&#39;m QianXiaomo. Nice to meet you!</p>
-                <p>Full stack engineer.</p>
-                <p className="invisible">-</p>
-                <p>My slogan:</p>
-                <ol className="list-inside list-disc text-sm font-[family-name:var(--font-geist-mono)]">
-                    <li className="mb-2">Read widely and travel extensively.</li>
-                    <li className="mb-2">Realize that the past cannot be changed, but know that the future can be pursued.</li>
-                    <li>Talk is cheap, show me the code.</li>
-                </ol>
-                <p className="invisible">-</p>
-                <p>Contact:</p>
-                <p>Email: <Link href="mailto:i@truimo.com" className="hover:underline active:underline">i@truimo.com</Link></p>
+            <main className="mx-auto max-w-4/5 lg:w-1/2">
+                <div className="grid grid-cols-1 gap-y-2 ">
+                    <p>Hello, I&#39;m QianXiaomo. Nice to meet you!</p>
+                    <p>Full stack engineer.</p>
+                    <p className="invisible">-</p>
+                    <p>My slogan:</p>
+                    <ol className="list-inside list-disc text-sm font-[family-name:var(--font-geist-mono)]">
+                        <li className="mb-2">Read widely and travel extensively.</li>
+                        <li className="mb-2">Realize that the past cannot be changed, but know that the future can be
+                            pursued.
+                        </li>
+                        <li>Talk is cheap, show me the code.</li>
+                    </ol>
+                    <p className="invisible">-</p>
+                    <p>Contact:</p>
+                    <p>Email: <Link href="mailto:i@truimo.com">i@truimo.com</Link></p>
+                </div>
             </main>
             <footer>
                 <div className="m-8 text-center">
-                    <p>Copyright &copy; 2024 <Link href="https://github.com/Truimo" target="_blank" className="hover:underline active:underline">Truimo</Link>. All Rights Reserved.</p>
+                    <p>Copyright &copy; 2024 <Link href="https://github.com/Truimo" target="_blank">Truimo</Link>. All Rights Reserved.</p>
                 </div>
             </footer>
         </div>
