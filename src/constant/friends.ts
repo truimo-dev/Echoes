cbafc94b5340c32e6e5b8bc235e5f7d425462531
Truimo <Xiaomo@20mo.cn>
2024-10-08 18:16:56 +0800

    feat: add new link

--- src/constant/friends.ts
+++ src/constant/friends.ts
@@ -11,6 +11,11 @@ const friends: FriendItem[] = [
         icon: 'https://q1.qlogo.cn/g?b=qq&k=G1ibIlnicR8Ij4CZO3BF6cVA&s=640',
         description: '喵薄荷',
         href: 'https://0u0.ren/'
+    }, {
+        title: '北枫',
+        icon: 'https://q1.qlogo.cn/g?b=qq&nk=41777657&s=640',
+        description: '记录一个人的生活点滴',
+        href: 'https://beifeng.me/'
     },
 ]
 
