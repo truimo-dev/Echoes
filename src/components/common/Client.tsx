1e3d51af2494a5830c9095fc1c50b8430633e2b5
Truimo <xiaomo@20mo.cn>
2024-11-18 01:37:11 +0800

    feat: tips

--- src/components/common/Client.tsx
+++ src/components/common/Client.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import type {CSSProperties} from 'react'
 import {useEffect, useState} from 'react'
-import Link from 'next/link'
+import NextLink from 'next/link';
+import {Callout, Link} from '@radix-ui/themes';
+import {InfoCircledIcon} from '@radix-ui/react-icons';
 
 function useHostname(): string {
     const [hostname, setHostname] = useState<string>('')
@@ -16,9 +17,9 @@ function useHostname(): string {
     return hostname
 }
 
-const styles: CSSProperties = {
-    borderBottom: '0.5px solid #bababa33'
-}
+// const styles: CSSProperties = {
+//     borderBottom: '0.5px solid #bababa33'
+// }
 
 const autoRedirectScript: string = `setTimeout(function () { window.open('https://www.qxm.me' + window.location.pathname, '_self') }, 2e3)`;
 
@@ -30,11 +31,18 @@ export function DeprecatedDomain() {
     }
 
     return (
-        <>
-            <div className="mx-auto py-4 max-w-4/5 lg:w-1/2 text-center" style={styles}>
-                <p className="text-base">This domain will be deprecated soon, please visit <Link href="https://www.qxm.me/" target="_self">QXM.ME</Link>.</p>
-            </div>
+        <div className="mx-auto pt-4 max-w-4/5 lg:w-1/2 text-center">
+            <Callout.Root color="orange" variant="surface">
+                <Callout.Icon>
+                    <InfoCircledIcon/>
+                </Callout.Icon>
+                <Callout.Text>
+                    This domain will be deprecated soon, please visit <Link asChild={true} href="#">
+                    <NextLink href="https://www.qxm.me/" target="_self">QXM.ME</NextLink>
+                </Link>.
+                </Callout.Text>
+            </Callout.Root>
             <script dangerouslySetInnerHTML={{__html: autoRedirectScript}}></script>
-        </>
+        </div>
     )
 }
