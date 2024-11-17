e7297fe7e45910ffabdf3fad0ddc2b1868f450c9
Truimo <xiaomo@20mo.cn>
2024-11-17 23:26:56 +0800

    fix: optimize import

--- src/app/(home)/diary/layout.tsx
+++ src/app/(home)/diary/layout.tsx
@@ -1,11 +1,12 @@
 import '@/styles/diary.css';
+import {ReactNode} from 'react';
 import {Header, Main, Footer} from '@/components/layout/Diary';
 import {Theme} from '@radix-ui/themes';
 
 export default function RootLayout({
     children,
 }: Readonly<{
-    children: React.ReactNode;
+    children: ReactNode;
 }>) {
     return (
         <Theme>
