398a5aa73bca105bd700b1be95966c33e0754c9c
Truimo <xiaomo@20mo.cn>
2024-11-19 15:57:45 +0800

    feat: metadata

--- src/app/(home)/diary/page.tsx
+++ src/app/(home)/diary/page.tsx
@@ -2,6 +2,8 @@ import {Icon} from '@/components/common/Icon';
 import type {Metadata} from 'next';
 
 export const metadata: Metadata = {
+    title: 'Diary',
+    description: 'There are my diary.',
     alternates: {
         canonical: 'https://www.qxm.me/diary',
     }
