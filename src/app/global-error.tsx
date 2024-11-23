5d7d296f0beb652009910b86159d12abfc1c0ba9
Truimo <xiaomo@20mo.cn>
2024-11-23 14:16:46 +0800

    feat: remove radix-ui

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import {useEffect} from 'react';
-import {Text} from '@radix-ui/themes';
 
 export default function GlobalError({ error }: {
     error: Error & { digest?: string }
@@ -13,10 +12,10 @@ export default function GlobalError({ error }: {
 
     return (
         <div className="grid justify-items-end h-screen place-content-center px-4 gap-2">
-            <Text as="p" className="uppercase tracking-widest">Service issues occurred</Text>
-            <Text as="p">
+            <p className="uppercase tracking-widest">Service issues occurred</p>
+            <p>
                 <button className="btn" onClick={() => location.reload()}>Retry</button>
-            </Text>
+            </p>
         </div>
     );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,12 @@
-import NextLink from 'next/link';
-import {Text, Link} from '@radix-ui/themes';
+import {Link} from '@/components/ui/link';
 
 export default function NotFound() {
     return (
         <div className="grid justify-items-end h-screen place-content-center px-4 gap-2">
-            <Text as="p" className="uppercase tracking-widest">404 | Not Found</Text>
-            <Text as="p">
-                <Link asChild={true} href="#" color="gray">
-                    <NextLink href="/">Go Back Home</NextLink>
-                </Link>
-            </Text>
+            <p className="uppercase tracking-widest">404 | Not Found</p>
+            <p>
+                <Link href="/">Go Back Home</Link>
+            </p>
         </div>
     );
 }
