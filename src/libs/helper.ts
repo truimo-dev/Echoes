6e8206cca9d8a76fea0f5f946e0b4a0b15bdada2
Truimo <xiaomo@20mo.cn>
2024-11-23 21:13:27 +0800

    fix: clsxm any

--- src/libs/helper.ts
+++ src/libs/helper.ts
@@ -1,10 +1,9 @@
 import clsx from 'clsx'
 import {twMerge} from 'tailwind-merge'
+import type {ClassNameValue} from 'tailwind-merge'
 
-const clsxm = (...args: any[]) => {
+const clsxm = (...args: ClassNameValue[]) => {
     return twMerge(clsx(args))
 }
-
-
 
 export { clsxm }
