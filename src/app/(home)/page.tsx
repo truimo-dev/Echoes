be010f0b38477f235fb1cea0ae538d0e297bb8e8
Truimo <xiaomo@20mo.cn>
2024-12-06 23:30:20 +0800

    feat: home page new content

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
 import {geistMono} from '@/fonts';
+import {clsxm} from '@/libs/helper';
 import {Link} from '@/components/ui/link';
 import type {Metadata} from 'next';
-import {PropsWithChildren} from 'react';
+import type {PropsWithChildren} from 'react';
 
 export const metadata: Metadata = {
     alternates: {
@@ -16,7 +17,7 @@ function Header() {
             <div className="flex items-end gap-x-4">
                 <div className="shrink-0">
                     <Image src="/avatar.jpg" alt="avatar" height="96" width="96"
-                           className="rounded-full w-24 h-24 aspect-square" priority></Image>
+                           className="rounded-xl w-24 h-24 aspect-square" priority></Image>
                 </div>
                 <div>
                     <h1 className="text-2xl font-bold my-2">Xiaomo Qian</h1>
@@ -27,9 +28,11 @@ function Header() {
     )
 }
 
-function Section(props: PropsWithChildren) {
+function Section(props: PropsWithChildren<{
+    className?: string
+}>) {
     return (
-        <section className="grid grid-cols-1 gap-y-2">
+        <section className={clsxm('grid grid-cols-1 gap-y-2', props.className)}>
             {props.children}
         </section>
     )
@@ -46,19 +49,23 @@ export default function Home() {
                         <p>My name is QianXiaomo (<span className="text-primary">QXM</span>, for short). I’m a software
                             developer.</p>
                     </Section>
-                    <Section>
-                        <p>My slogan:</p>
+                    <Section className="hidden">
+                        <h4>My slogan:</h4>
                         <ol className={`list-inside list-disc text-sm ${geistMono.className}`}>
                             <li className="mb-2">Read widely and travel extensively.</li>
-                            <li className="mb-2">Realize that the past cannot be changed, but know that the future can
-                                be
-                                pursued.
-                            </li>
+                            <li className="mb-2">Realize that the past cannot be changed, but know that the future can be pursued.</li>
                             <li>Talk is cheap, show me the code.</li>
                         </ol>
                     </Section>
                     <Section>
-                        <p>Contact:</p>
+                        <p>No matter who you are, I&#39;m glad you could come here.</p>
+                        <p>You won&#39;t get anything here. This website and I are just placeholders in both the digital
+                            and real worlds.</p>
+                        <p>...</p>
+                        <p>Maybe you can take a look around here.</p>
+                    </Section>
+                    <Section>
+                    <h4 id="contact">Contact:</h4>
                         <p>Email: <Link href="mailto:hi@xdm.me">hi@qxm.me</Link></p>
                     </Section>
                 </div>
