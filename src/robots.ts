5125ac52f1b9050b853bd7fdb3eaa6c0976cdb2b
Truimo <xiaomo@20mo.cn>
2024-12-07 20:52:09 +0800

    fix: robots.txt

--- src/robots.ts
+++ src/robots.ts
@@ -2,12 +2,26 @@ import type {MetadataRoute} from 'next'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: [
-                '/development/'
-            ],
-        },
+        rules: [
+            {
+                userAgent: 'MJ12bot',
+                disallow: '/'
+            }, {
+                userAgent: 'SemrushBot',
+                disallow: '/'
+            }, {
+                userAgent: 'dotbot',
+                disallow: '/'
+            }, {
+                userAgent: 'SMTBot',
+                disallow: '/'
+            }, {
+                userAgent: '*',
+                allow: '/',
+                disallow: [
+                    '/development/'
+                ],
+            },
+        ],
     }
 }
