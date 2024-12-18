de21c3b58918cda9039337d18de5c2e9f7292e0e
Truimo <xiaomo@20mo.cn>
2024-12-18 13:55:10 +0800

    feat: speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/index.css';
 
+import {SpeedInsights} from '@vercel/speed-insights/next';
 import type {PropsWithChildren} from 'react';
 import type {Metadata} from 'next';
 import {ThemeProvider} from '@/providers/theme-provider';
@@ -23,6 +24,7 @@ export default async function RootLayout(props: PropsWithChildren) {
                 <ThemeProvider>
                     {props.children}
                 </ThemeProvider>
+                <SpeedInsights/>
             </body>
         </html>
     );
