5e06003fa50c7367103cab3fccc3377d1de043a1
Truimo <xiaomo@20mo.cn>
2025-01-05 08:40:55 +0800

    fix: vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import {defineConfig, envField} from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
@@ -9,47 +9,49 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      nesting: true,
+    integrations: [
+        tailwind({
+            nesting: true,
+        }),
+        react(),
+    ],
+
+    output: 'server',
+
+    env: {
+        schema: {
+            VERCEL_ENV: envField.string({
+                context: 'server',
+                access: 'public',
+                optional: true,
+            }),
+            UPSTASH_REDIS_REST_TOKEN: envField.string({
+                context: 'server',
+                access: 'secret',
+            }),
+            UPSTASH_REDIS_REST_URL: envField.string({
+                context: 'server',
+                access: 'public',
+                default: 'https://example.upstash.io'
+            }),
+            NOTION_KEY: envField.string({
+                context: 'server',
+                access: 'secret',
+            }),
+            NOTION_DIARY_DATABASE_ID: envField.string({
+                context: 'server',
+                access: 'public',
+            }),
+            NOTION_IMAGES_DATABASE_ID: envField.string({
+                context: 'server',
+                access: 'public',
+            }),
+        }
+    },
+
+    adapter: vercel({
+        edgeMiddleware: true,
+        imageService: true,
+        isr: true,
     }),
-    react(),
-  ],
-
-  output: 'server',
-
-  env: {
-    schema: {
-      VERCEL_ENV: envField.string({
-        context: 'server',
-        access: 'public',
-        optional: true,
-      }),
-      UPSTASH_REDIS_REST_TOKEN: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
-      UPSTASH_REDIS_REST_URL: envField.string({
-        context: 'server',
-        access: 'public',
-        default: 'https://example.upstash.io'
-      }),
-      NOTION_KEY: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
-      NOTION_DIARY_DATABASE_ID: envField.string({
-        context: 'server',
-        access: 'public',
-      }),
-      NOTION_IMAGES_DATABASE_ID: envField.string({
-        context: 'server',
-        access: 'public',
-      }),
-    }
-  },
-
-  adapter: vercel({
-    edgeMiddleware: true,
-  }),
 });
