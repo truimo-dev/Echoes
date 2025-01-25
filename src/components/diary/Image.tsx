c3b8b84141bebe95fa3eb2cf8f7fe38361f3920a
Truimo <xiaomo@20mo.cn>
2025-01-26 00:39:17 +0800

    feat: image zoom

--- src/components/diary/Image.tsx
+++ src/components/diary/Image.tsx
@@ -1,3 +1,6 @@
+import {useEffect, useRef} from 'react'
+import mediumZoom from 'medium-zoom'
+
 interface ImageProps {
     className?: string
     src: string
@@ -5,9 +8,21 @@ interface ImageProps {
 }
 
 function Image(props: ImageProps) {
+    const imgRef = useRef<HTMLImageElement>(null)
+
+    useEffect(() => {
+        const zoom = mediumZoom(imgRef.current ?? void 0, {
+            background: 'var(--background)',
+        });
+
+        return () => {
+            zoom.detach();
+        }
+    }, [imgRef.current]);
+
     return (
-        <img className={props.className} src={props.src} alt={props.alt}
-             referrerPolicy='no-referrer' loading='lazy' />
+        <img src={props.src} alt={props.alt} className={props.className}
+             ref={imgRef} loading='lazy' referrerPolicy='no-referrer' />
     );
 }
 
--- src/components/notion/blocks/Image.tsx
+++ src/components/notion/blocks/Image.tsx
@@ -1,4 +1,6 @@
+import {useEffect, useRef} from 'react'
 import clsx from 'clsx'
+import mediumZoom from 'medium-zoom'
 import RichText from '@/components/notion/blocks/RichText'
 import {Block} from '@/components/notion/blocks/Block'
 import type {PropsWithChildren} from 'react'
@@ -20,8 +22,7 @@ function Image({block, children}: PropsWithChildren<{
     return (
         <Block>
             <figure className="max-w-full w-fit mx-auto">
-                <img src={url} alt={alt} loading='lazy' referrerPolicy='no-referrer'
-                     className={clsx('block max-w-full w-fit object-cover', styles.img)} />
+                <Img src={url} alt={alt} className={clsx('block max-w-full w-fit object-cover', styles.img)} />
                 {img.caption.length > 0 && (
                     <figcaption className="py-1 text-right break-words text-neutral-500 text-sm">
                         <RichText rich_text={img.caption}/>
@@ -29,6 +30,31 @@ function Image({block, children}: PropsWithChildren<{
                 )}
             </figure>
         </Block>
+    )
+}
+
+interface ImgProps {
+    src: string
+    alt: string
+    className?: string
+}
+
+function Img(props: ImgProps) {
+    const imgRef = useRef<HTMLImageElement>(null)
+
+    useEffect(() => {
+        const zoom = mediumZoom(imgRef.current ?? void 0, {
+            background: 'var(--background)',
+        });
+
+        return () => {
+            zoom.detach();
+        }
+    }, [imgRef.current]);
+
+    return (
+        <img src={props.src} alt={props.alt} className={props.className}
+             ref={imgRef} loading='lazy' referrerPolicy='no-referrer' />
     )
 }
 
