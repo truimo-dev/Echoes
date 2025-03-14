6c10d7a98060e7146d721692d3ead8fe5fcb0483
Truimo <Xiaomo@20mo.cn>
2025-03-14 18:17:27 +0800

    fix: styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import {defineConfig, envField} from 'astro/config';
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel';
 
