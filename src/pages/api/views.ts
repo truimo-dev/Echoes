2bca80cec44c5481d9ad578680b20df6874bb399
Truimo <Xiaomo@20mo.cn>
2025-04-13 12:50:38 +0800

    feat: views count

---
+++ src/pages/api/views.ts
@@ -0,0 +1,19 @@
+import type {APIRoute} from 'astro'
+import { VERCEL_ENV } from 'astro:env/server'
+import { redis } from '@/libs/redis'
+import { kvKeys } from '@/constant/kv'
+
+
+export const GET: APIRoute = async ({ request }) => {
+    let views: number = 261101
+
+    if (null === request.headers.get('referer')) {
+        views = -1
+    } else if (VERCEL_ENV === 'production') {
+        views = await redis.incr(kvKeys.totalPageViews)
+    }
+
+    return Response.json({
+        views: views
+    })
+}
