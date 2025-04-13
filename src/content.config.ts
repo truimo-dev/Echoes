05641a8ae813d49833ad178406bbd1be2c5b4666
Truimo <Xiaomo@20mo.cn>
2025-04-13 11:56:14 +0800

    feat: excerpts

---
+++ src/content.config.ts
@@ -0,0 +1,25 @@
+import { defineCollection, z } from 'astro:content'
+import { file } from 'astro/loaders'
+import { v5 as uuidv5 } from 'uuid'
+
+const excerpts = defineCollection({
+    loader: file('./data/excerpts.json', {
+        parser: (text) => {
+            const json = JSON.parse(text)
+            return json.list.map((it: { content: string, id?: string }) => {
+                it.id = uuidv5(it.content, json.uuid)
+                return it
+            })
+        }
+    }),
+    schema: z.object({
+        id: z.string(),
+        content: z.string(),
+        title: z.string().optional(),
+    })
+})
+
+
+export const collections = {
+    excerpts
+}
