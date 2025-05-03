5e7a6cdbd968dcc3bb120b5e841dc04109ad60d2
Truimo <xiaomo@20mo.cn>
2025-05-03 12:25:39 +0800

    feat: prerender
    (cherry picked from commit a990707b54b6d7bdf4012764456911c5500878e7)

--- src/types/content.ts
+++ src/types/content.ts
@@ -3,6 +3,9 @@ import type { CollectionEntry } from 'astro:content'
 interface Post extends CollectionEntry<'blog'> {
 }
 
+interface Diary extends CollectionEntry<'diary'> {
+}
+
 export type {
-    Post
+    Post, Diary
 }
--- src/utils/content.ts
+++ src/utils/content.ts
@@ -5,3 +5,8 @@ export async function getSortedPosts() {
     const posts = await getCollection('blog')
     return _.orderBy(posts, ['data.time'], ['desc'])
 }
+
+export async function getSortedDiary() {
+    const diary = await getCollection('diary')
+    return _.orderBy(diary, ['data.time'], ['desc'])
+}
